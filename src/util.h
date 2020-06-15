4779e5fe9e9ab382eda1ccf8336449b672789b08
Nico Lucciola <nico.lucciola@gmail.com>
2020-06-15 09:16:07 +0200

    merged bitcoin 0.17 changes

--- src/util.h
+++ src/util.h
@@ -67,13 +67,6 @@ inline std::string _(const char* psz)
 void SetupEnvironment();
 bool SetupNetworking();
 
-struct CLogCategoryActive
-{
-    std::string category;
-    bool active;
-};
-
-    };
 template<typename... Args>
 bool error(const char* fmt, const Args&... args)
 {
